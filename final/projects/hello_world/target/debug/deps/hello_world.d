a7b942a6fd9448eb573318b5d329ecd9c7f2fa7c
xiaotsai <49838210+xiaotsai@users.noreply.github.com>
2020-06-27 23:48:20 +0800

    ｇｏｏｄ

---
+++ final/projects/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+D:\上課\rust\projects\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+D:\上課\rust\projects\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ final/projects/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+D:\上課\rust\projects\hello_world\target\debug\hello_world.exe: D:\上課\rust\projects\hello_world\src\main.rs
